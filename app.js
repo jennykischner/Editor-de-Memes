3da3a6c74b1eea0f94a5232c7f6e62ae9962a92c
kischnerjennifer@gmail.com <kischnerjennifer@gmail.com>
2021-08-12 17:39:10 -0300

    Agregue JS abrir y cerrar paneles

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Abrir y cerrar paneles
+
+const main = document.querySelector(".mainPanel");
+const image = document.querySelector("#image");
+const text = document.querySelector("#text");
+const switchThemeButton = document.querySelector("#switchTheme");
+const closeImg = document.querySelector("#closeImg");
+const closeText = document.querySelector("#closeText");
+const imageEdit = document.querySelector(".imageEdit");
